97dbbb529b22c20cd931c3b1856085fe38493de6
hinha <martinuz.dawan9@gmail.com>
2022-09-09 17:15:19 +0700

    update middleware

--- middlewares/fiber_middleware.go
+++ middlewares/fiber_middleware.go
@@ -1,8 +1,6 @@
 package middlewares
 
 import (
-	"os"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 
@@ -10,19 +8,9 @@ import (
 	"github.com/hinha/coai/logger"
 )
 
-func FiberMiddleware(a *fiber.App, cfg *config.Config) {
-	fLog, err := os.OpenFile(cfg.Log.File.Path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		panic(err)
-	}
-
-	zl := logger.NewZap(fLog, cfg)
-	defer func() {
-		_ = zl.Sync()
-	}()
-
+func FiberMiddleware(a *fiber.App, cfg *config.Config, zap *logger.Logger) {
 	a.Use(
 		cors.New(),
-		NewLogger(Config{AppConfig: cfg, Logger: zl}),
+		NewLogger(Config{AppConfig: cfg, Logger: zap.Logger()}),
 	)
 }
