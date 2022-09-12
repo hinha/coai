7d901f00656e8759cd840e877c60e26cc3079465
hinha <martinuz.dawan9@gmail.com>
2022-09-12 11:14:29 +0700

    moving package

--- server/routes/private_routes.go
+++ server/routes/private_routes.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gofiber/contrib/otelfiber"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/hinha/coai/accounts/handlers"
+	userHandlers "github.com/hinha/coai/modules/user/handlers"
 )
 
 // PrivateRoutes func for describe group of private routes.
@@ -18,9 +18,9 @@ func PrivateRoutes(a *fiber.App) {
 	)
 
 	// handler Auth
-	authHandler := handlers.NewAuthHandler()
-	authGroup := route.Group("/auth")
-	authGroup.Get("/login", authHandler.SignIn).Name("authLogin")
+	userHandler := userHandlers.NewAuthHandler()
+	userGroup := route.Group("/auth")
+	userGroup.Get("/login", userHandler.SignIn).Name("authLogin")
 
 	route.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(map[string]interface{}{})
